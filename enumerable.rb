dfe776b034df1eef4f9f35f41e1cba3d8ff6e0b4
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-08-20 21:39:12 +0100

    Add my_each_with_index_method

--- enumerable.rb
+++ enumerable.rb
@@ -8,4 +8,14 @@ module Enumerable
     end
   end
 
+  def my_each_with_index
+    e = self.length - 1
+    if block_given?
+      0.upto(e) do |x|
+        yield(self[x],x)
+      end
+    end
+  end
 end
+
+%w(Ibrahimovic is better than Messi).my_each_with_index {|i,j| puts i+ " ====> "+ j.to_s}
