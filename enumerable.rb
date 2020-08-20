1561bbfe9782e29a82319921994e5b5557501d1a
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-08-20 22:56:47 +0100

    Make my_all, my_any, and my_none methods

--- enumerable.rb
+++ enumerable.rb
@@ -33,5 +33,48 @@ module Enumerable
     return arr
   end
 
+  def my_all?
+    var = self.to_a
+    e = var.size - 1
+    
+    if block_given?
+      0.upto(e) do |x|
+        unless yield(var[x])
+          return false
+        end
+      end
+    end
+    return true
+  end
+
+  def my_any?
+    var = self.to_a
+    e = var.size - 1
+    
+    if block_given?
+      0.upto(e) do |x|
+        if yield(var[x])
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def my_none?
+    var = self.to_a
+    e = var.size - 1
+    
+    if block_given?
+      0.upto(e) do |x|
+        if yield(var[x])
+          return false
+        end
+      end
+    end
+    return true
+  end
+
 end
 
+puts %w{ant bear cat ibrahimovic}.my_none? { |word| word.length >= 5 }
