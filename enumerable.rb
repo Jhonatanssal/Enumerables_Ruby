939ef255ff2c5060effc538444935a75c96a407f
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-08-21 20:48:46 +0100

    reduce linters errors

--- enumerable.rb
+++ enumerable.rb
@@ -2,23 +2,22 @@ module Enumerable
   def my_each
     self.to_a
     e = self.size - 1
-    if block_given?
-      0.upto(e) do |x|
-        yield(self[x])
-      end
-      self
+    return unless block_given?
+
+    0.upto(e) do |x|
+      yield(self[x])
     end
+    self
   end
 
   def my_each_with_index
     self.to_a
     e = self.size - 1
-    if block_given?
-      0.upto(e) do |x|
-        yield(self[x],x)
-      end
-      self
+    return unless block_given?
+    0.upto(e) do |x|
+      yield(self[x], x)
     end
+    self
   end
 
   def my_select
@@ -27,80 +26,65 @@ module Enumerable
     arr = []
     if block_given?
       0.upto(e) do |x|
-        if yield(self[x])
-          arr.push(self[x])
-        end
+        if yield(self[x]) arr.push(self[x])
       end
     end
-    return arr
+    arr
   end
 
   def my_all?
     self.to_a
     e = self.size - 1
-    
+
     if block_given?
       0.upto(e) do |x|
-        unless yield(self[x])
-          return false
-        end
+        unless yield(self[x]) return false
       end
     end
-    return true
+    true
   end
 
   def my_any?
     self.to_a
     e = self.size - 1
-    
+
     if block_given?
       0.upto(e) do |x|
-        if yield(self[x])
-          return true
-        end
+        if yield(self[x]) return true
       end
     end
-    return false
+    false
   end
 
   def my_none?
     self.to_a
     e = self.size - 1
-    
+
     if block_given?
       0.upto(e) do |x|
-        if yield(self[x])
-          return false
-        end
+        if yield(self[x]) return false
       end
     end
-    return true
+    true
   end
 
   def my_count(input = nil)
     self.to_a
     count = 0
-    
+
     if block_given?
       self.my_each do |x|
-        if yield(x)
-          count += 1 
-        end
+        if yield(x) count += 1
       end
       count
     end
     
-    if input.nil?
-      return self.length
-    end
+    if input.nil? return self.length
 
-    self.my_each do |x| 
-      if x == input
-        count += 1
-      end
+    self.my_each do |x|
+      if x == input { count += 1 }
     end
-    count 
-
+    count
   end
 
   def my_map
@@ -109,7 +93,7 @@ module Enumerable
     var.my_each do |x|
       arr.push(yield(x))
     end
-    return arr
+    arr
   end
 
   def my_inject(*arg)
@@ -117,41 +101,38 @@ module Enumerable
     b = var.size - 1
 
     if block_given?
-      if arg.size == 0
+      if arg.empty?
         acc = var[0]
         1.upto(b) do |i|
           acc = yield(acc, var[i])
         end
-        return acc
+        acc
       elsif arg.size == 1
         acc = arg[0]
         0.upto(b) do |i|
           acc = yield(acc, var[i])
         end
-        return acc
+        acc
       end
-    else
-      if arg.size == 1
-        acc = var[0]
-        1.upto(b) do |i|
-          acc = acc.send(arg[0],var[i])
-        end
-        return acc
-      elsif arg.size == 2
-        acc = arg[0]
-        0.upto(b) do |i|
-          acc = acc.send(arg[1],var[i])
-        end
-        return acc
+    elsif arg.size == 1
+      acc = var[0]
+      1.upto(b) do |i|
+        acc = acc.send(arg[0], var[i])
       end
+      acc
+    elsif arg.size == 2
+      acc = arg[0]
+      0.upto(b) do |i|
+        acc = acc.send(arg[1], var[i])
+      end
+      acc
     end
   end
-
 end
 
 def multiply_els(arr)
   arr.my_inject(:*)
 end
 
-print multiply_els([2,4,5])
+print multiply_els([2, 4, 5])
 puts
